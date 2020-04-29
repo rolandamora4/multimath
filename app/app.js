6e964fcc14dab2a9b24810ccf4dbeaba4e48fc92
ramora <ramora@renditiondigital.com.ph>
2020-04-29 18:55:45 +0800

    Exercise 2

--- app/app.js
+++
@@ -1,5 +0,0 @@
-function startGame() {
-    var messageElement = document.getElementById('messages');
-    messageElement.innerText = "Welcome to multimath";
-}
-document.getElementById('startGame').addEventListener('click', startGame);
